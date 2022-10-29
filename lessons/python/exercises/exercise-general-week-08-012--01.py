f834e852c579e22c6107725498c70cad6368f951
sh-navid <shamshirsaz.navid@live.com>
2022-10-29 21:37:06 +0330

    Update

--- lessons/python/exercises/exercise-general-week-08-012--01.py
+++ lessons/python/exercises/exercise-general-week-08-012--01.py
@@ -1,5 +1,8 @@
 from enum import Enum, auto
 
+# THIS IS NOT THE BEST PRACTICE
+# THIS IS JUST FOR TEACHING
+
 
 class Size(Enum):
     FREE_SIZE = auto()
@@ -32,33 +35,48 @@ class Brand(Enum):
     UNDEFINED = auto()
 
 
+class Reflect:
+    def __str__(self) -> str:
+        V=vars(self)
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+
 class Cloth:
-    name=""
-    size: Size
+    name = ""
     brand: Brand
 
 
 class Skirt(Cloth):
-    name="Skirt"
+    name = "Skirt"
 
 
 class Hat(Cloth):
-    name="Hat"
+    name = "Hat"
 
 
 class Pants(Cloth):
-    name="Pants"
+    name = "Pants"
 
 
 class Coat(Cloth):
-    name="Coat"
+    name = "Coat"
+
+
+class Variant:
+    size: Size
+    color: Color
 
 
 class Product:
     cloth: Cloth
-    count: int
-    color: Color
+    variant: Variant
 
 
 class Products:
-    collection:list(Product)
+    collection: list(Product)
+
+    @classmethod
+    def add(cls, name: str, size: list[Size], brand: Brand, color: Color, count: int):
+        cloth
