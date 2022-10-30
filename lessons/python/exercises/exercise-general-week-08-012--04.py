a7c0d2e58e5a6592e218431aa8d26691b8992d2e
sh-navid <shamshirsaz.navid@live.com>
2022-10-30 20:36:58 +0330

    Update

--- lessons/python/exercises/exercise-general-week-08-012--04.py
+++ lessons/python/exercises/exercise-general-week-08-012--04.py
@@ -1,4 +1,6 @@
-D = [
+import numpy as np
+
+D = np.array([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -6,10 +8,12 @@ D = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+])
 
 
-L = [6, 6, 6, 6, 6, 6, 6, 6, 6]
+L = [8, 8, 8, 8, 8, 8, 8, 8, 8]
 
 
 X = {
@@ -18,18 +22,44 @@ X = {
     2: '#',
 }
 
+p1, p2 = 1, 2
+
 
 def show():
     for row in D:
         for cell in row:
             print(X[cell], end="")
+            # print(cell, end="")
         print()
 
 
+def check_4(row):
+    if (np.sum(row) == 4):
+        print("@ is the winner")
+        exit()
+    if (np.sum(row) == 8):
+        print("# is the winner")
+        exit()
+
+
+def check_4_square(M):
+    for x in [0, 1, 2, 3]:
+        check_4(M[x])
+        check_4(M[:, x])
+    check_4(np.array([M[0, 0], M[1, 1], M[2, 2], M[3, 3]]))
+    check_4(np.array([M[0, 3], M[1, 2], M[2, 1], M[3, 0]]))
+
+
+def check_winner():
+    for row in range(0, 6):
+        for col in range(0, 6):
+            #  Div and Conq
+            M = D[row:row+4, col:col+4]
+            # print(M)
+            check_4_square(M)
+
+
 show()
-
-p1, p2 = 1, 2
-
 while True:
     i = int(input(f"Enter 1-9: "))
     if i >= 1 and i <= 9:
@@ -37,3 +67,4 @@ while True:
         L[i-1] -= 1
         p1, p2 = p2, p1
         show()
+        check_winner()
