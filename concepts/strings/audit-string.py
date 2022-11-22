e4b7dc319956cc9d5871554f91374eaf73bf5464
sh-navid <shamshirsaz.navid@live.com>
2022-11-22 16:53:22 +0330

    formatting text like upper, lower, casefold, ...

---
+++ concepts/strings/audit-string.py
@@ -0,0 +1,19 @@
+s = "Hello to this world"
+
+print("Swapcase: ", s.swapcase())
+
+print("Title: ", s.title())
+
+print("Upper: ", s.upper())
+
+print("Lower: ", s.lower())
+
+print("Casefold: ", "ß".casefold())  # Stronger than lower
+
+print("Capitalize: ", s.capitalize())
+
+print("Center: ", s.center(50))
+
+print("LJust: ", s.ljust(50))
+
+print("RJust: ", s.rjust(50))
