acbc8f3a2906c3fdd9f7198f157c09e06931cad8
sh-navid <shamshirsaz.navid@live.com>
2022-11-22 19:47:39 +0330

    Update

--- concepts/strings/other-string-functions.py
+++ concepts/strings/other-string-functions.py
@@ -1,3 +1,11 @@
-print("Hello\tWorld".expandtabs(16))
+print("Hello\tWorld".expandtabs(2))
+# Hello World
 
+print("Hello\tWorld".expandtabs(8))
+# Hello   World
+
+print("Hello\tWorld".expandtabs(16))
 # Hello           World
+
+print("Hello\tWorld".expandtabs(32))
+# Hello                           World
