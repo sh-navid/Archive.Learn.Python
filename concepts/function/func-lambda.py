3f31f4bab9c9d263ee93b8f08d1778ffec431b65
sh-navid <shamshirsaz.navid@live.com>
2022-11-22 17:43:16 +0330

    Update

---
+++ concepts/function/func-lambda.py
@@ -0,0 +1,15 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Simple lambda function
+my_function = lambda: print("this is a lambda function")
+
+
+my_function()
+my_function()
+my_function()
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Lambda function with arguments
+calculate = lambda a,b,c: a*b*c
+
+print(calculate(12,13,14))
