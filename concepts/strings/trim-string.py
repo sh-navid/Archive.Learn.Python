6338da8822e01e7a6386fb9da1bc16a1d4d9aee1
sh-navid <shamshirsaz.navid@live.com>
2022-11-22 16:52:57 +0330

    trim, strip

---
+++ concepts/strings/trim-string.py
@@ -0,0 +1,11 @@
+# strip, lstrip, rstrip
+
+###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+s = "   Hello   "
+print("<", s.strip(), ">")
+print("<", s.lstrip(), ">")
+print("<", s.rstrip(), ">")
+
+# < Hello >
+# < Hello    >
+# <    Hello >
